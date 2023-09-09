34f0e772578f36bea2448f32733c8301e002f7bb
mune-kyun <rzknurfaizi@gmail.com>
2023-09-09 21:04:24 +0700

    finish landing page??

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,11 @@
     <div
       class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent z-50"
     >
-      <div class="bg-slate-800 bg-opacity-80 p-4 rounded-lg min-w-[500px]">
-        <h1 class="text-white text-2xl font-bold text-center">HoloWaifu</h1>
+      <div
+        class="bg-slate-800 bg-opacity-80 p-4 rounded-lg min-w-[500px] flex flex-col items-center gap-2"
+      >
+        <h1 class="text-white text-5xl font-bold text-center">HoloWaifu</h1>
+        <enter-button />
       </div>
     </div>
     <div class="flex gap-3 items-center justify-center">
@@ -19,9 +22,9 @@
 </template>
 
 <script>
+import EnterButton from "./components/EnterButton.vue";
 export default {
+  components: { EnterButton },
   name: "App",
 };
 </script>
-
-<style></style>
---
+++ src/components/EnterButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="text-white text-lg underline font-semibold" @click="goIn">
+    ENTER ➔
+  </button>
+</template>
+
+<script>
+export default {
+  name: "EnterButton",
+  methods: {
+    goIn() {
+      const event = new CustomEvent("InterComponent", {
+        detail: {
+          customData: {
+            from: "landing-component",
+            action: "press-enter",
+          },
+        },
+      });
+      dispatchEvent(event);
+    },
+  },
+};
+</script>
